13c1d1ad4d3e09d0193b595350fcdee19550d911
Adaías Fuentes <adaias.fuentes@banregio.com>
2019-02-06 17:56:32 -0600

    Se agrega log y gitignore

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
-//@Log4j
+@Log4j2
 public class DemoController {
 
 	@PostMapping
 	public Model save(Model model){
+		log.info("Guardando " + model.getNombre());
 		return model;
 	}
 	
 	@GetMapping
 	public Model find(Model model){
 		Model m = new Model();
+		log.info("Hola " + model.getNombre());
 		m.setNombre("Hola "+model.getNombre());
 		return m;
 	}
